3962724ccb8afa71c5f66e335d56f2ca74d858a5
xwj924195077 <xwj6115@163.com>
2016-12-21 21:14:24 +0800

    create

---
+++ onlinemove-dao/src/com/ssm/dao/impl/UserDaoImpl.java
@@ -0,0 +1,47 @@
+package com.ssm.dao.impl;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ssm.dao.UserDao;
+import com.ssm.vo.User;
+
+@Repository("userDao")
+public class UserDaoImpl extends SqlSessionDaoSupport implements UserDao{
+	
+	@Autowired
+	@Override
+public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+	super.setSqlSessionFactory(sqlSessionFactory);
+}	
+	@Override
+	public void save(User user) {
+		getSqlSession().insert("add",user);
+	}
+
+	@Override
+	public void update(User user) {
+		getSqlSession().update("update", user);
+	}
+
+	@Override
+	public void delete(int id) {
+		getSqlSession().delete("delete",id);
+	}
+
+	@Override
+	public User findById(int id) {
+		User user =getSqlSession().selectOne("findById",id);
+		return user;
+	}
+
+	@Override
+	public List<User> findAll() { 
+		return getSqlSession().selectList("findAll");
+	}
+
+}
