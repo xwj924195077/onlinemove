620fb656cacf773059c1e1258a288e1a9e74c8c6
xwj924195077 <xwj6115@163.com>
2016-12-25 18:48:16 +0800

    sss

--- onlinemove-dao/src/com/ssm/controller/UserController.java
+++ onlinemove-dao/src/com/ssm/controller/UserController.java
@@ -1,75 +1,131 @@
 package com.ssm.controller;
 
 import java.util.List;
-import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.ssm.dao.UserDao;
+import com.ssm.fr.BasicController;
+import com.ssm.fr.Constant;
+import com.ssm.fr.ResponseObj;
 import com.ssm.vo.User;
 
 @Controller
-public class UserController {
+@RequestMapping("/user")
+public class UserController extends BasicController {
 
 	@Autowired
 	private UserDao userDao;
 
-	@RequestMapping("/xx")
+	@RequestMapping(value = "/getAllUser", method = RequestMethod.POST)
 	@ResponseBody
-	public void saveUsers(@RequestBody List<String> list) {
-		System.out.println("**************************");
-	}
-
-	@RequestMapping("/list")
-	public String list(HttpServletRequest request, Model model) {
-		List<User> list = userDao.findAll();
-		System.out.println(list);
-		model.addAttribute("list", list);
-		return "list.jsp";
-	}
-
-	@RequestMapping("/add")
-	public String add(HttpServletRequest request, User user) throws Exception {
-		request.setCharacterEncoding("utf-8");
-		user = new User();
-		String name = request.getParameter("name");
-		System.out.println(name);
-		String pwd = request.getParameter("pwd");
-		if (name != null && pwd != null) {
-			user.setName(name);
-			user.setPwd(pwd);
-			userDao.save(user);
-			System.out.println(user);
-			return "redirect:/list.do";
-		} else {
-			return "add.jsp";
+	public String getAllUser() {
+		List<User> list = null;
+		ResponseObj responseObj = new ResponseObj();;
+		responseObj.setCode(Constant.ERROR);
+		try {
+			list = userDao.findAll();
+			responseObj.setCode(Constant.SUCCESS);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		responseObj.setResult(list);
+		return JSON.toJSONString(responseObj);
+	}
+	
+	@RequestMapping(value = "/findOneUserById", method = RequestMethod.POST)
+	@ResponseBody
+	public String list(@RequestBody String request) {
+		User user = null;
+		ResponseObj responseObj = new ResponseObj();;
+		responseObj.setCode(Constant.ERROR);
+		try {
+			JSONObject json = JSON.parseObject(request);
+			int id = (int) json.get("data");
+			user = userDao.findById(id);
+			responseObj.setCode(Constant.SUCCESS);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		responseObj.setResult(user);
+		return JSON.toJSONString(responseObj);
+		
+		
 	}
 
-	@RequestMapping("/edit")
-	public ModelAndView edit(@RequestParam(value = "id") int id) {
-		ModelAndView mv = new ModelAndView("edit.jsp");
-		User user = userDao.findById(id);
-		mv.addObject("user", user);
-		return mv;
+	@RequestMapping(value = "/createUser", method = RequestMethod.POST)
+	@ResponseBody
+	public String add(@RequestBody String request) throws Exception {
+
+		System.out.println(request);
+		ResponseObj responseObj = new ResponseObj();
+		responseObj.setCode(Constant.ERROR);
+		try {
+			JSONObject json = JSON.parseObject(request);
+			Map<String, Object> data = (Map<String, Object>) json.get("data");
+			if (null != data) {
+				User user = new User();
+				BeanUtils.populate(user, data);
+				userDao.save(user);
+				responseObj.setCode(Constant.SUCCESS);
+				responseObj.setResult(Constant.TRUE);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			responseObj.setResult(Constant.FALSE);
+		}
+		return JSON.toJSONString(responseObj);
+
 	}
 
-	@RequestMapping("/update")
-	public String update(HttpServletRequest request, User user) {
-		userDao.update(user);
-		return "redirect:/list.do";
-	}
+	@RequestMapping(value = "/updateUser", method = RequestMethod.POST)
+	@ResponseBody
+	public String update(@RequestBody String request) throws Exception {
 
-	@RequestMapping("/delete")
-	public String delete(int id) {
-		userDao.delete(id);
-		return "redirect:/list.do";
+		ResponseObj responseObj = new ResponseObj();
+		responseObj.setCode(Constant.ERROR);
+		try {
+			JSONObject json = JSON.parseObject(request);
+			Map<String, Object> data = (Map<String, Object>) json.get("data");
+			if (null != data) {
+				User user = new User();
+				BeanUtils.populate(user, data);
+				userDao.update(user);
+				responseObj.setCode(Constant.SUCCESS);
+				responseObj.setResult(Constant.TRUE);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			responseObj.setResult(Constant.FALSE);
+		}
+		return JSON.toJSONString(responseObj);
+	}
+	
+	@RequestMapping(value = "/deleteUser", method = RequestMethod.POST)
+	@ResponseBody
+	public String delete(@RequestBody String request) {
+		ResponseObj responseObj = new ResponseObj();
+		responseObj.setCode(Constant.ERROR);
+		try {
+			JSONObject json = JSON.parseObject(request);
+			int id = (int) json.get("data");
+			userDao.delete(id);
+			responseObj.setCode(Constant.SUCCESS);
+			responseObj.setResult(Constant.TRUE);
+		} catch (Exception e) {
+			e.printStackTrace();
+			responseObj.setResult(Constant.FALSE);
+		}
+		return JSON.toJSONString(responseObj);
 
 	}
 
